abcdd0253279ce52b8f605f8f2e2ef3b4d608bff
frogger111 <tobiasz.parys@gmail.com>
2014-02-23 13:26:20 +0100

    Intial commit from local git

---
+++ CocoaPodsTest/CocoaPodsTest.h
@@ -0,0 +1,13 @@
+//
+//  CocoaPodsTest.h
+//  CocoaPodsTest
+//
+//  Created by Tobiasz Parys on 23/02/14.
+//  Copyright (c) 2014 Treadstone Entertainment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaPodsTest : NSObject
+
+@end
---
+++ CocoaPodsTest/CocoaPodsTest.m
@@ -0,0 +1,13 @@
+//
+//  CocoaPodsTest.m
+//  CocoaPodsTest
+//
+//  Created by Tobiasz Parys on 23/02/14.
+//  Copyright (c) 2014 Treadstone Entertainment. All rights reserved.
+//
+
+#import "CocoaPodsTest.h"
+
+@implementation CocoaPodsTest
+
+@end
